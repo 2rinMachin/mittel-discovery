db7016ffab33353696b0401611e55fa5f4110f51
nik-kotine <mswv280407@gmail.com>
2025-10-05 18:23:10 -0500

    feat: add postman collection

--- app/main.py
+++ app/main.py
@@ -21,11 +21,3 @@ app.add_middleware(
 )
 
 app.include_router(routes.router)
-
-# simple internal server error handler
-# @app.exception_handler(Exception)
-# async def general_exception_handler(request: Request, e: Exception):
-#     return JSONResponse(
-#         status_code = 500,
-#         content = { "error" : "Internal server error" }
-#     )
